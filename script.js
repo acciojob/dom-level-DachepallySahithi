bf1bed4aab75fb987e74c7eb07786d971dc1d805
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-21 20:33:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const levelElement = document.getElementById("level");
+function findDOMLevel(element, level = 0) {
+	if(element === null){
+		return level;
+	}
+	return findDOMLevel(element.parentElement, level + 1);
+}
+const domLevel = findDOMLevel(levelElement);
+alert('The level of the element is: ${domLevel}')
